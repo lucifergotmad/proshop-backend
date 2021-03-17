2cc3c84700c4284450d847165f4f24703638407a
L u c i f e r <amateur.fullstackdev@gmail.com>
2021-03-17 20:19:19 +0700

    setting up user authentication, jwt etc

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import productRouters from "./routes/productRoutes.js";
+import userRouters from "./routes/userRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -12,11 +13,14 @@ connectDB();
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
 app.use("/api/products", productRouters);
+app.use("/api/users", userRouters);
 
 app.use(notFound);
 
