6ccbee2d0a446c4c2b97c72665e66cbba66f0854
L u c i f e r <amateur.fullstackdev@gmail.com>
2021-03-23 17:37:52 +0700

    adding paypal and many more

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import productRouters from "./routes/productRoutes.js";
 import userRouters from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,6 +22,11 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRouters);
 app.use("/api/users", userRouters);
+app.use("/api/orders", orderRoutes);
+
+app.get("/api/config/paypal", (req, res) =>
+  res.send(process.env.PAYPAL_CLIENT_ID)
+);
 
 app.use(notFound);
 
